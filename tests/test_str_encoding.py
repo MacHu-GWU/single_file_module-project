b426b67fea27dd9c5866aa2d04476027219081ce
MacHu-GWU <husanhe@gmail.com>
2017-02-10 22:58:36 -0500

    2017-02-10

--- tests/test_str_encoding.py
+++ tests/test_str_encoding.py
@@ -2,48 +2,36 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+import re
+import string
 import pytest
 from sfm import str_encoding
 
+
 def test_base64_urlsafe():
     for before in [
-            r"C:\windows",
-            "a_b_c!@#$%^&*()",
-            "中文英文日文",
-        ]:
+        r"C:\windows",
+        "a_b_c!@#$%^&*()",
+        "中文英文日文",
+    ]:
         after = str_encoding.encode_base64_urlsafe(before)
         assert before == str_encoding.decode_base64_urlsafe(after)
 
 
-def test_compress_decompress():
-    s = \
-    """
-    Beautiful is better than ugly.
-    Explicit is better than implicit.
-    Simple is better than complex.
-    Complex is better than complicated.
-    Flat is better than nested.
-    Sparse is better than dense.
-    Readability counts.
-    Special cases aren't special enough to break the rules.
-    Although practicality beats purity.
-    Errors should never pass silently.
-    Unless explicitly silenced.
-    In the face of ambiguity, refuse the temptation to guess.
-    There should be one-- and preferably only one --obvious way to do it.
-    Although that way may not be obvious at first unless you're Dutch.
-    Now is better than never.
-    Although never is often better than *right* now.
-    If the implementation is hard to explain, it's a bad idea.
-    If the implementation is easy to explain, it may be a good idea.
-    Namespaces are one honking great idea -- let's do more of those!
-    """
+def test_hexstr():
+    for before in [
+        r"C:\windows",
+        "a_b_c!@#$%^&*()",
+        "中文英文日文",
+    ]:
+        after = str_encoding.encode_hexstr(before)
+        assert before == str_encoding.decode_hexstr(after)
 
-    s1 = str_encoding.compress_str(s)
-    assert len(s1) < len(s)
-    assert s == str_encoding.decompress_str(s1)
+    s = "中文英文日文"
+    s1 = str_encoding.encode_hexstr(s)
+    assert len(set(s1).difference("0123456789abcdef")) == 0
     
-
+    
 if __name__ == "__main__":
     import os
     pytest.main([os.path.basename(__file__), "--tb=native", "-s", ])
---
+++ tests/test_winzip.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, time
+import pytest
+from sfm import winzip
+
+
+def test_winzip():
+    winzip.zip_a_folder(os.getcwd(), "1.zip")
+    winzip.zip_everything_in_a_folder(os.getcwd(), "2.zip")
+    winzip.zip_many_files([__file__,], "3.zip")
+    
+    # temp file will be removed in 20 seconds
+    time.sleep(20.0)
+    for p in ["1.zip", "2.zip", "3.zip"]:
+        try:
+            os.remove(p)
+        except:
+            pass
+    
+        
+if __name__ == "__main__":
+    import os
+    pytest.main([os.path.basename(__file__), "--tb=native", "-s", ])
